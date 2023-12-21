a422d0a80998ef656e1c9e0460dcb1d4f0cc920c
KevanshGopani <kevanshgopani5@gmail.com>
2023-12-21 21:46:13 +0530

    added basic ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="h-[100vh] flex justify-center items-center text-6xl">
+      Welcome to the Alphabin Consultancy
+    </h1>
+  );
+};
+
+export default App;
