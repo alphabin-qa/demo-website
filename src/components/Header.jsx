bf79c7e6d1cc9853fb27e402f1fd1ab0ce3077b8
KevanshGopani <kevanshgopani5@gmail.com>
2023-12-21 23:13:24 +0530

    added navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,12 +13,12 @@ const Header = () => {
           <p className="font-extrabold text-[28.61px] leading-[42.91px] tracking-[1.06px]">
             Alphabin
           </p>
-          <p className="font-normal text-[14.58px] leading-[21.87px] tracking-[4.2px]">
+          <p className="font-bold text-[14.58px] leading-[21.87px] tracking-[4.2px]">
             DEMO STORE
           </p>
         </div>
         <div>
-          <ul className="flex justify-center items-center gap-8 font-medium text-[16px] leading-5">
+          <ul className="flex justify-center items-center gap-8 font-medium text-[16px] leading-5 font-inter">
             <li className="cursor-pointer p-[2px]">Home</li>
             <li className="cursor-pointer p-[2px]">About Us</li>
             <li className="cursor-pointer p-[2px]">Contact Us</li>
