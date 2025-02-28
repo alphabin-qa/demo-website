9c06902626c9551f1839c26e1041cf0b5c485a43
Alphabin Technology Consulting <83493276+alphabin-01@users.noreply.github.com>
2025-02-28 16:11:11 +0530

    Refine CORS configuration to specific allowed origins

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
-    // origin: ['https://demo.alphabin.co', 'http://localhost:3000', 'https://backend.demo.alphabin.co'],
+    origin: ["https://demo.alphabin.co", "http://localhost:3000"],
     credentials: true,
   })
 );
