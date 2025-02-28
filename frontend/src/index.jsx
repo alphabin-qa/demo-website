e96066ec74fd860aaf1f525922c3f0d236b88fd9
Alphabin Technology Consulting <83493276+alphabin-01@users.noreply.github.com>
2025-02-28 16:29:36 +0530

    Switch from HashRouter to BrowserRouter with basename

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import App from "./App";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./store";
 
@@ -10,8 +10,8 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <ReduxProvider store={store}>
-    <HashRouter>
+    <BrowserRouter basename="/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </ReduxProvider>
 );
